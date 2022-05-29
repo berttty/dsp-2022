66dec916eb409fcaa133e00bea36c14450a94419
bertty <bertty@apache.org>
2022-05-29 22:39:51 +0200

    add phases seed, those required be implemented
    Signed-off-by: bertty <bertty@apache.org>

---
+++ src/ramu/ramu/phases/grid_generation.py
@@ -0,0 +1,29 @@
+from ramu.phase import Phase
+
+class GridGeneration(Phase):
+
+    def inputFormatter(self) -> Callable[[str], I]:
+        """
+        inputFormatter provide a method to convert the text to the type that could be process
+        by the Phase
+
+        this method need to be implemented
+        :return: Callable that will be use by the map function
+        """
+        return None
+
+    def outputFormatter(self) -> Callable[[O], str]:
+        """
+        outputFormatter provide a method to convert content of RDD into text file
+
+        this method need to be implemented
+        :return: Callable that will be use as the convertor before to store
+        """
+        return None
+
+    def run(self, rdd: RDD[I]) -> RDD[O]:
+        """
+        run is the method that contains the logic of the phase
+        :param rdd: the rdd that will use as source
+        :return: return the rdd after the elements converted
+        """
